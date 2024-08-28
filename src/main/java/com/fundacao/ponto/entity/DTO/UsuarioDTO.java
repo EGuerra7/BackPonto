bb3e8f9e73091d087fe9ea00709ec5fd6f7d6e43
Erick Guerra <erickmuzzi@gmail.com>
2024-08-28 18:03:27 -0300

    Tentativa 1

--- src/main/java/com/fundacao/ponto/entity/DTO/UsuarioDTO.java
+++ src/main/java/com/fundacao/ponto/entity/DTO/UsuarioDTO.java
@@ -17,9 +17,4 @@ public class UsuarioDTO {
     private String email;
     private String senha;
     private String permissao;
-
-    public UsuarioDTO(String email, String senha) {
-        this.email = email;
-        this.senha = senha;
-    }
 }
