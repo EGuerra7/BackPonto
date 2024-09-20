ad098b498323eb8c0c93378743d999008e8b9e87
Erick Guerra <164356036+EGuerra7@users.noreply.github.com>
2024-09-20 09:44:25 -0300

    allow!

--- src/main/java/com/fundacao/ponto/CORS/WebConfig.java
+++ src/main/java/com/fundacao/ponto/CORS/WebConfig.java
@@ -14,7 +14,7 @@ public class WebConfig implements WebMvcConfigurer {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/**")
-                        .allowedOrigins("https://backponto.onrender.com")
+                        .allowedOrigins("https://front-ponto-olive.vercel.app")
                         .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                         .allowedHeaders("*")
                         .allowCredentials(true);
