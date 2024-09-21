9b4a7760680d3b61aa7eeeaf29c656b68fafe456
EGuerra7 <erickmuzzi@gmail.com>
2024-09-21 11:22:32 -0300

    Deploy banco de dados AWS

--- src/main/java/com/fundacao/ponto/PontoApplication.java
+++ src/main/java/com/fundacao/ponto/PontoApplication.java
@@ -1,20 +1,21 @@
 package com.fundacao.ponto;
 
 import org.modelmapper.ModelMapper;
-import org.springframework.boot.SpringApplication;
+import org.springframework.boot.WebApplicationType;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.context.annotation.Bean;
+
 
 @SpringBootApplication
 public class PontoApplication {
-
 	public static void main(String[] args) {
-		SpringApplication.run(PontoApplication.class, args);
+		new SpringApplicationBuilder(PontoApplication.class).run(args);
 	}
 
-	@Bean
-	public ModelMapper getModelMapper() {
-		return new ModelMapper();
-	}
+@Bean
+public ModelMapper getModelMapper() {
+	return new ModelMapper();
+}
 
 }
--- src/main/java/com/fundacao/ponto/repository/PontoRepository.java
+++ src/main/java/com/fundacao/ponto/repository/PontoRepository.java
@@ -1,10 +1,10 @@
 package com.fundacao.ponto.repository;
 
-import com.fundacao.ponto.entity.DTO.PontoDTO;
+
 import com.fundacao.ponto.entity.Ponto;
-import com.fundacao.ponto.entity.Usuario;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
+
 
 import java.time.LocalDate;
 import java.util.List;
