03ff274e3ba831640e87726c05aae50455cb9338
Erick Guerra <erickmuzzi@gmail.com>
2024-09-23 14:00:49 -0300

    MySQL remoto 1.4

--- src/test/java/com/fundacao/ponto/PontoApplicationTests.java
+++ src/test/java/com/fundacao/ponto/PontoApplicationTests.java
@@ -3,10 +3,9 @@ package com.fundacao.ponto;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+
 class PontoApplicationTests {
 
-	@Test
 	void contextLoads() {
 	}
 
