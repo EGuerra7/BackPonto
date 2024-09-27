0f695c38be418da2928c5c5fba9682072911a234
Erick Guerra <erickmuzzi@gmail.com>
2024-09-27 16:41:48 -0300

    Ponto 2.1

--- src/test/java/com/fundacao/ponto/PontoApplicationTests.java
+++ src/test/java/com/fundacao/ponto/PontoApplicationTests.java
@@ -3,7 +3,7 @@ package com.fundacao.ponto;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-
+@SpringBootTest
 class PontoApplicationTests {
 
 	void contextLoads() {
